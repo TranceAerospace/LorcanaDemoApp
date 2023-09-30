5f39a61fa72e22b70e5d1712fa84ef6d8f167e7e
Trance <xp46290@gmail.com>
2023-09-29 20:47:21 -0400

    Initial Commit

---
+++ LorcanaTestApp/LorcanaTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LorcanaTestAppApp.swift
+//  LorcanaTestApp
+//
+//  Created by Mark Trance on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LorcanaTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
